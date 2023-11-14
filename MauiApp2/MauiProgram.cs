2782c5956656dea3098f67d7eb66cbd48eb8c80d
Daniel Armstrong <33160370+armstrongdj@users.noreply.github.com>
2023-11-14 11:52:47 -0600

    Add project and solution

---
+++ MauiApp2/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Raygun4Maui;
+using Telerik.Maui.Controls.Compatibility;
+
+namespace MauiApp2
+{
+    public static class MauiProgram
+    {
+        private const string ApiKey = "YOUR KEY HERE";
+
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseTelerik()
+                .AddRaygun4Maui(ApiKey)
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
